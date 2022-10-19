7b62dae2a13c63178a08cac626357ff8640bd80d
LEONARDO DA SILVA <leogs2003@gmail.com>
2022-10-19 12:06:32 -0300

    atualizado

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,15 +1,17 @@
-importe  Reagir  de  'reagir' ;
-importe  ReactDOM  de  'react-dom' ;
-import  './index.css' ;
-importar  App  de  './App' ;
-importar  reportWebVitals  de  './reportWebVitals' ;
-ReactDOM . renderizar (
-< Reagir . Modo Estrito >,
-< App  / >,
-< / Reagir . StrictMode > ,
-documento . getElementById ( 'root' )
-) ;
-// Se você quiser começar a medir o desempenho em seu aplicativo, passe uma função
-// para registrar resultados (por exemplo: reportWebVitals(console.log))
-// ou enviar para um endpoint de análise. Saiba mais: https://bit.ly/CRA-vitals
-reportWebVitals ( ) ;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
